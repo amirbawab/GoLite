3abfe75a0df4cdd65eabf80066b16d1342a98e50
Jeremie <jeremie@weboxigen.com>
2018-03-05 19:23:03 -0500

    impl function

--- src/declarables/function.cpp
+++ src/declarables/function.cpp
@@ -44,5 +44,8 @@ void golite::Function::symbolTablePass(golite::SymbolTable *root) {
 
     golite::SymbolTable* fn_symbol_table = new golite::SymbolTable();
     root->addChild(fn_symbol_table);
-    // TODO : go through all statements
+
+    for(golite::Statement* stmt: this->block_->getStatements()) {
+        stmt->symbolTablePass(fn_symbol_table);
+    }
 }
