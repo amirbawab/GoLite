6eba96dd3b5a27fb2c2175dbbc1f2f7dbce2a65e
Amir Bawab <amirbawab@gmail.com>
2018-03-11 21:07:50 -0400

    Added type check for append

--- src/expression/append.cpp
+++ src/expression/append.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <golite/identifier.h>
 #include <golite/primary_expression.h>
+#include <iostream>
 
 std::string golite::Append::toGoLite(int indent) {
     std::stringstream ss;
@@ -34,8 +35,18 @@ void golite::Append::weedingPass() {
 golite::TypeComponent* golite::Append::typeCheck() {
     TypeComponent* left_expression_type = left_expression_->typeCheck();
     TypeComponent* right_expression_type = right_expression_->typeCheck();
-    // TODO
-    return nullptr;
+    std::vector<TypeComposite*> left_children = left_expression_type->getChildren();
+    std::vector<TypeComposite*> right_children = right_expression_type->getChildren();
+    if(left_children.size() != 2 || !left_children[1]->isSlice()) {
+        golite::Utils::error_message("Append statement expects slice type as first argument but given "
+                                     + left_expression_type->toGoLiteMin(), left_children[1]->getLine());
+    }
+    if(right_children.size() != 1 || !left_children[0]->isCompatible(right_children[0])) {
+        golite::Utils::error_message("Append statement arguments are not compatible "
+                                     + left_expression_type->toGoLiteMin() + " and "
+                                     + right_expression_type->toGoLiteMin(), getLine());
+    }
+    return left_expression_type;
 }
 
 void golite::Append::symbolTablePass(SymbolTable *root) {
