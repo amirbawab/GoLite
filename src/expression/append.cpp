bb62683b63e3c8e457f2686947a30dc7663002cf
Amir Bawab <amirbawab@gmail.com>
2018-03-13 15:51:01 -0400

    Fixed bug in append

--- src/expression/append.cpp
+++ src/expression/append.cpp
@@ -35,7 +35,7 @@ void golite::Append::weedingPass() {
 golite::TypeComponent* golite::Append::typeCheck() {
     TypeComponent* left_expression_type = left_expression_->typeCheck();
     TypeComponent* right_expression_type = right_expression_->typeCheck();
-    std::vector<TypeComposite*> left_children = left_expression_type->getChildren();
+    std::vector<TypeComposite*> left_children = left_expression_type->resolveChildren();
     std::vector<TypeComposite*> right_children = right_expression_type->getChildren();
     if(left_children.size() != 2 || !left_children[1]->isSlice()) {
         golite::Utils::error_message("Append statement expects slice type as first argument but given "
