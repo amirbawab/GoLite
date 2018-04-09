3e432fca5f37578a97061c5a39ccd172a75e6e60
Amir Bawab <amirbawab@gmail.com>
2018-04-09 18:10:34 -0400

    Fixed bug

--- src/lib/ts_helper.cpp
+++ src/lib/ts_helper.cpp
@@ -116,13 +116,17 @@ std::string golite::TSHelper::codeSlice(int indent) {
        << golite::Utils::indent(indent) << "class Slice<T> {" << std::endl
        << golite::Utils::indent(indent+1) << "capacity : number = 0;" << std::endl
        << golite::Utils::indent(indent+1) << "array : Array<T> = new Array<T>(0);" << std::endl
-       << golite::Utils::indent(indent+1) << "append = (val : T) : Slice<T> => {" << std::endl
+       << golite::Utils::indent(indent+1) << "append = (val : any) : Slice<T> => {" << std::endl
        << golite::Utils::indent(indent+2) << "var slice : Slice<T> = this.clone();" << std::endl
        << golite::Utils::indent(indent+2) << "if(slice.array.length + 1 > slice.capacity) {" << std::endl
        << golite::Utils::indent(indent+3) << "slice.capacity = (slice.capacity + 1) * 2;" << std::endl
        << golite::Utils::indent(indent+3) << "slice.array = slice.array.clone();" << std::endl
        << golite::Utils::indent(indent+2) << "}" << std::endl
-       << golite::Utils::indent(indent+2) << "slice.array.push(val);" << std::endl
+       << golite::Utils::indent(indent+2) << "if(val instanceof Object) {" << std::endl
+       << golite::Utils::indent(indent+3) << "slice.array.push(val.clone());" << std::endl
+       << golite::Utils::indent(indent+2) << "} else {" << std::endl
+       << golite::Utils::indent(indent+3) << "slice.array.push(val);" << std::endl
+       << golite::Utils::indent(indent+2) << "}" << std::endl
        << golite::Utils::indent(indent+2) << "return slice;" << std::endl
        << golite::Utils::indent(indent+1) << "}" << std::endl
        << golite::Utils::indent(indent+1) << "check = (index : number) : Array<T> => {" << std::endl
