7031d0d396db262b1f82ab8a879fc2589fb52401
Jeremie <jeremie@weboxigen.com>
2018-04-11 00:48:28 -0400

    tired

--- programs/3-semantics+codegen/valid/Swap-1.go
+++ programs/3-semantics+codegen/valid/Swap-1.go
@@ -1,5 +1,10 @@
+//~5 4 3 2 1
 package main
 
 func main() {
-	x, x := 0, 1
+	// 1. All expressions on the right hand side are well-typed
+	a, b, c, d, e := 1, 2, 3, 4, 5
+	a, b, c, d, e = e, d, c, b, a
+
+	println(a, b, c, d, e)
 }
